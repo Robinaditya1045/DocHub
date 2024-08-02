c4b5471ec0ba7f1d55702064c45f2d2dca4a588a
Robin <mahamuniaditya07@gmail.com>
2024-08-02 23:26:20 +0530

    initial commit with a basic foundation of the project

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Document = require("./Document");
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+  dbName: "docs"
+}).then(() => {
+    console.log("MongoDB connected")
+  }).catch(err => {
+    console.error("MongoDB connection error:", err)
+  });
+
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const defaultValue = "";
+
+io.on("connection", socket => {
+  socket.on("get-document", async documentId => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-document", async data => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+});
+
+async function findOrCreateDocument(id) {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+}
